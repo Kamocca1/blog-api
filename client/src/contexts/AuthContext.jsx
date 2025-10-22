54cc60abdc6fc2b8447c1d9da0f4729119a292ab
Kamocca <86765533+Kamocca1@users.noreply.github.com>
2025-10-22 22:07:12 +0200

    feat(client): initialize React app
    - Set up initial Vite React project structure for client
    - Added AuthContext for authentication state management
    - Implemented useAuth hook for handling auth logic
    - Configured ESLint with custom rules for React
    - Added project files: .gitignore, index.html, main entry points, and vercel rewrite config

---
+++ client/src/contexts/AuthContext.jsx
@@ -0,0 +1,16 @@
+import { createContext } from "react";
+import useAuth from "../hooks/useAuth.jsx";
+
+const AuthContext = createContext(null);
+
+function AuthProvider({ children }) {
+    const { user, setUser, isLoading } = useAuth();
+
+    return (
+        <AuthContext.Provider value={{ user, setUser, isLoading }}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
+
+export { AuthContext, AuthProvider };
---
+++ client/src/hooks/useAuth.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+
+function useAuth() {
+    const [user, setUser] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        async function checkLoginStatus() {
+            try {
+                setIsLoading(true);
+                await new Promise((resolve) => setTimeout(resolve, 1000));
+                const response = await fetch(
+                    `${import.meta.env.VITE_API_URL}/auth/session`,
+                    {
+                        method: "GET",
+                        credentials: "include",
+                    }
+                );
+
+                const data = await response.json();
+                if (data.user) {
+                    setUser(data.user);
+                } else {
+                    setUser(null);
+                }
+                setIsLoading(false);
+            } catch (err) {
+                console.error("Error with login status: ", err);
+                setIsLoading(false);
+                setUser(null);
+            }
+        }
+        checkLoginStatus();
+    }, []);
+
+    return { user, setUser, isLoading };
+}
+
+export default useAuth;
